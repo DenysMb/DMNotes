d27214659e46c1da2fa9a0c1013acf4aac654dc7
Denys Madureira <denysmb@zoho.com>
2022-03-31 14:28:38 -0300

    Add logout function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { logout } from "../../firebase";
 import useUser from "../../hooks/useUser";
 import Icon from "../Icon";
 import Styles from "./Header.module.scss";
@@ -9,7 +10,7 @@ const Header = () => {
   return (
     <div className={Styles.Header}>
       <h1 className={Styles.HeaderTitle}>DMNotes</h1>
-      <div className={Styles.HeaderUser}>
+      <div className={Styles.HeaderUser} onClick={logout}>
         <h3 className={Styles.HeaderUserName}>{user?.name}</h3>
         <div className={Styles.HeaderUserIcon}>
           <Icon name="right-to-bracket" />
